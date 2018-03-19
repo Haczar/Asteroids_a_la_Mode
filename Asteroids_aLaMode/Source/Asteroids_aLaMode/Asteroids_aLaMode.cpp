225d44f0f0011ff1fe992ab851d501875baa0712
Ed94 <edwardgz@gmail.com>
2018-03-19 07:18:02 -0400

    It was a long night. Enjoy. Don't worry nothing is gone just defaults changed.
    Most stuff is unimplemented. Works pretty much like it did before just has some structure to it some more fleshed out input options and some planned objects setup, with a nice level to test on.

--- Asteroids_aLaMode/Source/Asteroids_aLaMode/Asteroids_aLaMode.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Asteroids_aLaMode.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Asteroids_aLaMode, "Asteroids_aLaMode" );
